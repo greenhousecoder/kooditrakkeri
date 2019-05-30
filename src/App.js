c2da2a59afdd81aba49298717c7691f8b6b2d3f1
Teemu Vesterinen <teemuvest@fastmail.fm>
2019-05-30 17:08:28 +0300

    gh-pages try

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component} from 'react';
-import {  BrowserRouter as Router,  Route} from 'react-router-dom';
-import './App.css';
-import './components/Koodilaskuri/koodilaskuri.css';
-import Menu from './components/Menu/Menu.js';
-import Header from './components/Header.js';
-import Koodilaskuri from './components/Koodilaskuri/Koodilaskuri';
-import Graafi from './components/Graafi/Graafi.js';
-import Add from './components/Add/Add.js';
-import testdata from './components/testdata';
+import React, { Component} from 'react';  
+import {  BrowserRouter as Router,  Route} from 'react-router-dom';  // reititys
+import './App.css'; //Tuodaan tyyliä -> App.js 
+import './components/Koodilaskuri/koodilaskuri.css'; // Tuodaan tyyliä -> Koodilaskuri.js
+import Menu from './components/Menu/Menu.js'; //Tuodaan Alavalikko
+import Header from './components/Header.js'; //Tuodaan otsikko
+import Koodilaskuri from './components/Koodilaskuri/Koodilaskuri'; //Tuodaan Koodilistaus 
+import Graafi from './components/Graafi/Graafi.js'; //Tuodaan Pylväskaavio
+import Add from './components/Add/Add.js'; //Tuodaan koodinlisäys
+import testdata from './components/testdata'; //Sisältää kaiken datan
 
 
 //https://react-icons.netlify.com/#/icons/io  <-- tuolta löytyy ikonit
